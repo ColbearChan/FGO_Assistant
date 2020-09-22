e7739e66ec6d9fce670c9ef41afa90b82fa79385
HiuNgaiChan <62755325+HiuNgaiChan@users.noreply.github.com>
2020-09-22 22:03:43 +0100

    Update fgo_auto.py

--- main/fgo_auto.py
+++ main/fgo_auto.py
@@ -5,36 +5,44 @@ import time
 from classes import saberCasterTeam
 from classes import archerLancerTeam
 from classes import archerArcherTeam
+from classes import baserkerTeam
 from common import common
 
 #setup.py bdist_msi for installer in cx_freeze
 
-#common.apples()
-#time.sleep(3)
-#i = int(input('输入次数: '))
-#while i > 0:
-##    common.reTop()
-##    common.choosingAlly()
-##    time.sleep(3)
-    #saberCasterTeam.main()
-    #common.endingBit()
-    #time.sleep(2)
-    #i = i - 1
 
-roles = {1 : archerLancerTeam, 2 : archerArcherTeam, 3 : saberCasterTeam}
 
-def main(choices, numberOfRounds):
+#roles = {1 : archerLancerTeam, 2 : archerArcherTeam, 3 : saberCasterTeam, 5: baserkerTeam}
+roles = {1: baserkerTeam}
+
+
+def main(choices, numberOfRounds, stamina, maxStamina):
+	curStamina = stamina
 	numberOfRound = numberOfRounds
+	if curStamina < 40:
+		eatGoldApples()
+		curStamina += maxStamina
+		time.sleep(5)
+	common.reTop()
 	while numberOfRound > 0:
 		print('还有' + str(numberOfRound) + '次')
-		common.reTop()
 		common.choosingAlly()
-		time.sleep(3)
+		#time.sleep(3)
 		choices.main()
 		common.endingBit()
-		time.sleep(5)
-		numberOfRound = numberOfRound - 1
-
+		#time.sleep(5)
+		numberOfRound -= 1
+		curStamina -= 40
+		if numberOfRound != 0:
+			if curStamina < 40:
+				common.notReStart()
+				eatGoldApples()
+				curStamina += maxStamina
+				time.sleep(5)
+				common.reTop()
+			else:
+				common.reStart()
+	print('-=-结束-=-')
 
 def eatGoldApples():
 	common.goldApples()
